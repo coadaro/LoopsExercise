2a08c42716e1d4553340142fd4c133911a59aa75
“coadaro” <coadaro@gmail.com>
2022-03-15 12:00:24 -0400

    LoopsExercise

---
+++ script.js
@@ -0,0 +1,43 @@
+//1
+for (i = 1; i <= 7; i++){
+    console.log(i);
+};
+
+//2
+for (i = 0; i <= 25; i += 4){
+    console.log(i);
+};
+
+//3a
+const wizards = [
+    `Harry Potter`,
+    `Hermione Granger`,
+    `Ron Weasley`
+];
+
+//3b
+for (friends of wizards){
+    console.log(friends);
+};
+
+//4a
+let harryPotterMovies = 0;
+
+//4b
+while (harryPotterMovies <= 8){
+
+    //4c
+  console.log(harryPotterMovies);
+  harryPotterMovies++;
+};
+
+//bonus
+//5a
+const hogwartsHouses = [
+    `Gryffindor`,
+    `Hufflepuff`,
+    `Ravenclaw`,
+    `Slytherin`
+]
+
+//5b
