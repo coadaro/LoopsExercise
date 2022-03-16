4c57db2c624ddbd0cda1fec2b9617a3c5a7d1734
“coadaro” <coadaro@gmail.com>
2022-03-16 10:58:38 -0400

    LoopsExerciseWithBonus

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ for (friends of wizards){
 let harryPotterMovies = 0;
 
 //4b
-while (harryPotterMovies <= 8){
-
-    //4c
-  console.log(harryPotterMovies);
-  harryPotterMovies++;
+while (harryPotterMovies < 8){
+    harryPotterMovies++;
 };
+
+//4c
+console.log(harryPotterMovies);
 
 //bonus
 //5a
@@ -38,6 +38,43 @@ const hogwartsHouses = [
     `Hufflepuff`,
     `Ravenclaw`,
     `Slytherin`
-]
+];
 
 //5b
+for (list of hogwartsHouses){
+    for (letters of list){
+        console.log(letters);
+    };
+}
+
+//6a
+const quote = [
+    `yer`,
+    `A`,
+    `Wizard`,
+    `Harry`
+];
+
+//6b
+// console.log(...quote);
+//or
+let magicQuote = ``;
+for (i = 0; i < quote.length; i++) {
+    magicQuote += ` ${quote[i]}`
+};
+magicQuote = magicQuote.trim();
+console.log(magicQuote);
+
+//7
+//question inspired by fizzbuzz
+for (i=1; i<=25; i++){
+    if (i%3 === 0 && i%5 ===0){
+        console.log(`Expecto Patronum`);
+    } else if (i%3 === 0){
+        console.log(`Expecto`);
+    } else if (i%5 === 0){
+        console.log(`Patronum`);
+    } else {
+        console.log(i);
+    }
+}
